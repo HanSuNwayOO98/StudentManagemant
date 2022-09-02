1965adbf42c241ac28ee7e62765cb401c3831430
HanSuNwayOO98 <111266040+HanSuNwayOO98@users.noreply.github.com>
2022-09-02 07:57:31 +0630

    Add files via upload

---
+++ UserBean.java
@@ -0,0 +1,47 @@
+package studentmanagement.model;
+
+public class UserBean {
+	private String userId;
+	private String userName;
+	private String email;
+	private String password;
+	private String confirmPassword;
+	private String userRole;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
+}
